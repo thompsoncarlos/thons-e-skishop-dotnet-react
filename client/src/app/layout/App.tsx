6865e51ac15f2a4c4956b1b64c9a19347cefb657
Carlos Thompson <Carlos.Thompson@criticalsoftware.com>
2022-12-30 10:17:53 +0000

    Stop Section 5-6

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,71 @@
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import 'react-toastify/dist/ReactToastify.css';
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+import BasketPage from "../../features/basket/BasketPage";
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
+
+function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType  = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  if (loading) return <LoadingComponent message='Initialising app...' />
+
+  return (
+    <ThemeProvider theme={theme}>
+    <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+    <CssBaseline />
+    <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/catalog' component={Catalog} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
+        <Route path='/basket' component={BasketPage} />
+        <Route path='/checkout' component={CheckoutPage} />
+      </Switch>
+    </Container>
+  </ThemeProvider>
+  );
+}
+export default App;
